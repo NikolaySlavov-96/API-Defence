af641aad03bc2ff93e9c1eb3383e78de494dfbe6
NikolaySlavov-96 <nikolay.slavov.96@gmail.com>
2023-06-23 15:38:28 +0300

    adding field in model

--- models/CommentarModel.js
+++ models/CommentarModel.js
@@ -3,7 +3,13 @@ const { model, Schema, Types: { ObjectId } } = require('mongoose');
 const commentarSchema = new Schema({
     productId: { type: ObjectId, ref: 'User', required: true },
     ownerId: { type: ObjectId, ref: 'User', require: true },
+    name: { type: String, required: true },
     commentar: { type: String, required: true },
-    createAt: { type: new Date, required: true },
+    createAt: { type: Date, required: true },
+    lastUpdate: { type: Date, required: true },
     isDelete: { type: Boolean, default: false },
 });
+
+const Comment = model('Comment', commentarSchema);
+
+module.exports = Comment;
