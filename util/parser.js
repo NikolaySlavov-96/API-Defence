b0f7df1a8ed4d9091fe3f28fa1554a4d64485ce2
NikolaySlavov-96 <nikolay.slavov.96@gmail.com>
2023-06-28 08:28:27 +0300

    correction on  date creator

--- util/parser.js
+++ util/parser.js
@@ -10,14 +10,10 @@ function errorParser(err) {
 
 function createNewDate() {
     const date = new Date();
-    const month = date.getMonth() + 1;
-    const day = date.getDate();
-    const year = date.getFullYear();
-
     return date;
 }
 
 module.exports = {
     errorParser,
-    createNewDate
+    createNewDate,
 }
