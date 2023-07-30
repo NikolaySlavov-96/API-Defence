c0a645cf7b7892a8bc1ed8beb60362b7e194a7bf
NikolaySlavov-96 <nikolay.slavov.96@gmail.com>
2023-07-30 23:02:34 +0300

    change port and adding https module

--- env.js
+++ env.js
@@ -1,5 +1,5 @@
 module.exports = {
-    PORT: 3000,
+    PORT: 5055,
     DB_ADDRESS: 'mongodb://192.168.88.50/',
     DB_NAME: 'apiDefence',
     JWT_Secret: '4g6j1k7'
--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+const https = require('https');
+const fs = require('fs');
+
 const express = require('express');
 
 const { PORT } = require('./env');
@@ -6,11 +9,18 @@ const database = require('./config/database');
 const expres = require('./config/expres');
 const router = require('./config/router');
 
+/*
+const options = {
+    key: fs.readFileSync('../key-PRK.pem'),
+    cert: fs.readFileSync('../cert-CRT.pem')
+}
+*/
 
 start();
 
 async function start() {
     const app = express();
+    // https.createServer(options, app).listen(PORT, () => console.log('Server work on port ' + PORT));
 
     await database(app);
     expres(app, express);
