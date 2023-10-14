2a8b296bdc4155aad83afe028fed4f65a0e3b124
NikolaySlavov-96 <nikolay.slavov.96@gmail.com>
2023-10-14 19:14:04 +0300

    change DB address

--- env.js
+++ env.js
@@ -1,6 +1,6 @@
 module.exports = {
     PORT: 5055,
-    DB_ADDRESS: 'mongodb://192.168.88.50/',
+    DB_ADDRESS: 'mongodb://192.168.88.51/',
     DB_NAME: 'apiDefence',
     JWT_Secret: '4g6j1k7'
 }
