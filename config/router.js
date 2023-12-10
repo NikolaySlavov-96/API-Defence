b368e9ce0b35ca4a7995e3bd09dffbec877663b3
NikolaySlavov-96 <nikolay.slavov.96@gmail.com>
2023-12-10 20:44:02 +0200

    using likes functionality

--- config/router.js
+++ config/router.js
@@ -2,10 +2,12 @@ const auth = require("../routes/users");
 const user = require("../routes/profile");
 const source = require("../routes/source");
 const search = require("../routes/search");
+const like = require("../routes/like");
 
 module.exports = (app) => {
     app.use('/auth', auth);
     app.use('/user', user);
     app.use('/source', source);
     app.use('/search', search);
+    app.use('/like', like);
 }
