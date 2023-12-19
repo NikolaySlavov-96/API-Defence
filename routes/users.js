1aac91cc90602da0b80ffc4d8784cecc6d2f3feb
NikolaySlavov-96 <nikolay.slavov.96@gmail.com>
2023-12-19 09:52:55 +0200

    adding new password requirements

--- routes/users.js
+++ routes/users.js
@@ -8,9 +8,12 @@ const { isGuest, hasUser } = require('../middlewares/guards');
 users.post('/register',
     isGuest(),
     body('email').isEmail().withMessage('Emais is not corret'),
-    body('username').notEmpty().withMessage('Username is required'),
     body('username').isLength({ min: 6 }).withMessage('Username minimal size is 6 characters'),
-    body('password').isLength({ min: 5 }).withMessage('Password is not doesn\'t long'),
+    body('password').isLength({ min: 8 }).withMessage('Invalid password').bail()
+        .matches(/(?=.*\d)(?=.*[a-z])(?=.*[A-Z])((?=.*\W)|(?=.*_))^[^ ]+$/).withMessage('Incorrect type of password'),
+    body('rePassword').custom((value, { req }) => {
+        return value == req.body.password;
+    }).withMessage('Password missmatch!'),
     body('year').notEmpty().withMessage('Years is requited'),
     userController.createUser);
 
